362f1ae1756822f1e17083744de08feefa9e298a
riverjoy <tosakarinmylove@gmail.com>
2022-12-26 21:29:15 +0800

    deploy

---
+++ precache-manifest.f26b4653fe323508294c6c2d475a9051.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3bfdf8783032fc1767e",
+    "url": "/drizzle-accountBook/css/app.bf07a29e.css"
+  },
+  {
+    "revision": "e8cca1a1f625bee540e5ccb0630edb1d",
+    "url": "/drizzle-accountBook/index.html"
+  },
+  {
+    "revision": "a3bfdf8783032fc1767e",
+    "url": "/drizzle-accountBook/js/app.7bf9ba19.js"
+  },
+  {
+    "revision": "3e1c4097db025b3bfa57",
+    "url": "/drizzle-accountBook/js/chunk-vendors.06bf64f7.js"
+  },
+  {
+    "revision": "b5a7a21e1a508dd4afdb180acc51fdd1",
+    "url": "/drizzle-accountBook/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/drizzle-accountBook/robots.txt"
+  }
+]);
